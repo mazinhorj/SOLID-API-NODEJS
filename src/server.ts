a1efa883b6430f18e43e98d65855c028345ea632
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-06-11 12:36:12 -0300

    estrutura preparada

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { app } from './app'
+import { env } from './env'
+
+const theAuthor: string = 'Osmar Menezes da Silva (Mazinho)'
+
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log(
+      `-> Desenvolvido por ${theAuthor} \n-> ✅ Servidor rodando em http://localhost:${env.PORT}`,
+    )
+  })
